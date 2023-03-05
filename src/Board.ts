d56232b25259829b7b6ffe3e2ce84d110d151169
pultzlucas <pultzlucas@gmail.com>
2023-03-05 00:26:42 -0300

    Updates

--- src/Board.ts
+++ src/Board.ts
@@ -102,7 +102,7 @@ export default class Board {
         const pieceCaseEl = this.getPieceCaseElement(piece.x, piece.y)
         const img = document.createElement('img')
         img.classList.add('piece-img')
-        img.src = `${location.hostname}/assets/${piece.id}-${piece.team}.png`
+        img.src = `${location.href}/assets/${piece.id}-${piece.team}.png`
         pieceCaseEl.appendChild(img)
     }
 
