ddedc31f348ff734f3db3930c2ee0b70c5eee9ec
pultzlucas <pultzlucas@gmail.com>
2023-03-05 00:22:48 -0300

    Updates

--- dist/index.js
+++ dist/index.js
@@ -5,6 +5,7 @@ chessGame.startGame();
 document.querySelectorAll('.piece-case').forEach(pieceCase => {
     pieceCase.addEventListener('click', clickOverChessCase);
 });
+console.log(location.href);
 export function clickOverChessCase(e) {
     const target = e.target;
     let caseElement = target.classList.contains('piece-case') ? target : target.parentNode;
